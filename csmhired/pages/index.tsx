06ac420f494b7397190eca4ac61b615d6ab7b7c2
Sand Moshi <sandmoshi@gmail.com>
2022-10-01 00:24:00 -0400

    Updated homepage styling

--- csmhired/pages/index.tsx
+++ csmhired/pages/index.tsx
@@ -1,20 +1,21 @@
 // imports
-import Head from 'next/head'
-import Image from 'next/image'
-import NavBar from '../components/navbar/navbar'
-import SearchBar from '../components/searchBar/searchBar'
-import RowItem from '../components/rowItem/rowItem'
-import QuickFiltersRow from '../components/quickFiltersRow/quickFiltersRow'
-import icon1 from '../example/images/logo-svgrepo-com.svg';
-import icon2 from '../example/images/logo-svgrepo-com (1).svg';
-import icon3 from '../example/images/logo-svgrepo-com (2).svg';
-import { navLinks } from '../data/constants';
+import Head from "next/head";
+import Image from "next/image";
+import NavBar from "../components/navbar/navbar";
+import SearchBar from "../components/searchBar/searchBar";
+import RowItem from "../components/rowItem/rowItem";
+import QuickFiltersRow from "../components/quickFiltersRow/quickFiltersRow";
+import icon1 from "../example/images/logo-svgrepo-com.svg";
+import icon2 from "../example/images/logo-svgrepo-com (1).svg";
+import icon3 from "../example/images/logo-svgrepo-com (2).svg";
+import tada from "../data/images/svg/partypopper-svgrepo-com.svg";
+import { navLinks } from "../data/constants";
 
 // type imports
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 
 // style imports
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
@@ -26,36 +27,42 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <NavBar 
-          links = {navLinks}
-        />
-        <h1 className={styles.title}>
-          <strong>CSM</strong>hired
-        </h1>
-        <h3 style={{fontStyle: 'italic'}}>Helping CSMs find their dream job</h3>
-        <h2>Find your next customer success job here.</h2>
-
-        <div className={styles.searchBar}>
-          <SearchBar/>
+        <NavBar links={navLinks} />
+        <div className={styles.mainBackground}>
+          <div className={styles.wave}></div>
+          <div className={styles.tada}>
+            <Image src={tada} alt="tada!" height="450" width="450" />
+          </div>
         </div>
-        <div className={styles.quickFiltersRow} >
+        <h1 className={styles.title}>
+          <span>CSM</span>hired
+        </h1>
+        <h2 className={styles.tagline}>Helping CSMs find their dream job</h2>
+        {/* <h2 style={{fontStyle: 'italic'}}>Find your next customer success job here.</h2> */}
+
+        <div className={styles.searchBarContainer}>
+          <SearchBar />
+        </div>
+        <div className={styles.quickFiltersRow}>
           <QuickFiltersRow />
         </div>
-        <RowItem 
-          title="Company A"
-          description="Better perks, Better pay. Company A."
-          icon={icon1}
-        />
-        <RowItem 
-          title="Company B"
-          description="My perks better, my training better, my pay better. Everything better!"
-          icon={icon2}
-        />
-        <RowItem 
-          title="Company C"
-          description="My perks better, my training better, my pay better. Everything better!"
-          icon={icon3}
-        />
+        <div className={styles.postingsContainer}>
+          <RowItem
+            title="Company A"
+            description="Better perks, Better pay. Company A."
+            icon={icon1}
+          />
+          <RowItem
+            title="Company B"
+            description="My perks better, my training better, my pay better. Everything better!"
+            icon={icon2}
+          />
+          <RowItem
+            title="Company C"
+            description="My perks better, my training better, my pay better. Everything better!"
+            icon={icon3}
+          />
+        </div>
       </main>
 
       <footer className={styles.footer}>
@@ -64,14 +71,14 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
