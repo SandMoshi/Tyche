d4085706ea13f97b2235dacbc6162bc3c9a9fe74
Sand Moshi <sandmoshi@gmail.com>
2022-10-16 15:42:23 -0400

    Implemented blog row components. Updated blog page with examples. Added social media images

--- csmhired/components/blog/blogRowItem/blogRowItem.tsx
+++ csmhired/components/blog/blogRowItem/blogRowItem.tsx
@@ -1,33 +1,45 @@
 // VENDOR imports
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
+
+// imports
+import mountainImage from "../../../example/images/pexels-photo-572897.jpeg";
 
 // style imporst
 import styles from "./blogRowItem.module.css";
 
 type Props = {
-  image?: string;
+  image?: StaticImageData;
   imageAltText?: string;
   title: string;
   description?: string;
-  date?: string,
-  readTime?: string,
+  date?: string;
+  readTime?: string;
+  blankImage?: boolean;
+  postId: string;
 };
 
 const BlogRowItem = (props: Props) => {
   return (
     <div className={styles.container}>
-      {props.image && (
+      <a href={`/${props.postId}`}>
         <div className={styles.imageContainer}>
-          <Image
-            src={props.image}
-            height="256"
-            alt={props.imageAltText || "blog post image header"}
-          />
+          {props.blankImage ? (
+            <div style={{ background: "#d3c4cf", height: "256px" }}></div>
+          ) : (
+            <Image
+              src={props.image || mountainImage}
+              height="256"
+              alt={props.imageAltText || "blog post image header"}
+            />
+          )}
         </div>
-      )}
-      <h2 className={styles.firstLine}>{props.title}</h2>
-      <p className={styles.secondLine}>{props.description || ""}</p>
-      <p className={styles.thirdLine}><span>{props.date}</span><span>{props.readTime}</span></p>
+        <h2 className={styles.firstLine}>{props.title}</h2>
+        <p className={styles.secondLine}>{props.description || ""}</p>
+        <p className={styles.thirdLine}>
+          <span>{props.date}</span>
+          <span>{props.readTime} read</span>
+        </p>
+      </a>
     </div>
   );
 };
--- csmhired/pages/blogOnly.tsx
+++ csmhired/pages/blogOnly.tsx
@@ -7,6 +7,9 @@ import { navLinks } from "../data/constants";
 import tada from "../data/images/svg/partypopper-svgrepo-com.svg";
 import { BlogRowItem } from "../components/blog/blogRowItem/blogRowItem";
 import mountainImage from "../example/images/pexels-photo-572897.jpeg";
+import fbLogo from "../data/images/svg/facebook-svgrepo-com.svg";
+import instagramLogo from "../data/images/svg/instagram-svgrepo-com.svg";
+import linkedinLogo from "../data/images/svg/linkedin-svgrepo-com.svg";
 
 // type imports
 import type { NextPage } from "next";
@@ -16,7 +19,7 @@ import styles from "../styles/blogOnly.module.css";
 
 const BlogOnly: NextPage = () => {
   return (
-    <div className={styles.background}>
+    <div>
       <div className={styles.mainBackground}>
         <div className={styles.wave}></div>
         <div className={styles.tada}>
@@ -30,15 +33,44 @@ const BlogOnly: NextPage = () => {
         <h1 className={styles.miniBold}>BLOG</h1>
       </div>
       <h2 className={styles.tagline}>Helping CSMs find their dream job</h2>
+      <div className={styles.socialMediaLinks}>
+        <a href="https://www.linkedin.com/company/csmhired/">
+          <Image src={linkedinLogo} alt="CSM hired LinkedIn page" title="CSM hired social media" />
+        </a>
+        <a href="https://www.facebook.com/CSMhired">
+          <Image src={fbLogo} alt="CSM hired Facebook page" title="CSM hired social media" />
+        </a>
+        <a href="https://www.instagram.com/csmhired/">
+          <Image src={instagramLogo} alt="CSM hired Instagram page" title="CSM hired social media" />
+        </a>
+      </div>
       <div className={styles.postsContainer}>
         <BlogRowItem
           image={mountainImage}
-          title="Sand"
+          title="Example Title - Do something great, be bold."
           description="Donec dolor augue, posuere et congue eget, tincidunt eu eros. Morbi ornare ornare massa sed placerat. Nulla cursus, diam eu euismod varius, massa tortor fermentum felis, vitae consequat orci ex sed ex."
           date="Oct 2022"
           readTime="5 mins"
+          postId={"001"}
+        />
+        <BlogRowItem
+          blankImage
+          image={mountainImage}
+          title="Example Title - Do something great, be bold."
+          description="Donec dolor augue, posuere et congue eget, tincidunt eu eros. Morbi ornare ornare massa sed placerat. Nulla cursus, diam eu euismod varius, massa tortor fermentum felis, vitae consequat orci ex sed ex."
+          date="Oct 2022"
+          readTime="5 mins"
+          postId={"002"}
+        />
+        <BlogRowItem
+          title="Example Title - Do something great, be bold."
+          description="Donec dolor augue, posuere et congue eget, tincidunt eu eros. Morbi ornare ornare massa sed placerat. Nulla cursus, diam eu euismod varius, massa tortor fermentum felis, vitae consequat orci ex sed ex."
+          date="Oct 2022"
+          readTime="5 mins"
+          postId={"003"}
         />
       </div>
+      <div className={styles.background}></div>
     </div>
   );
 };
