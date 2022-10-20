f44014e66711dab273b87292ed83a8b47bc5f5ec
Sand Moshi <sandmoshi@gmail.com>
2022-10-19 22:44:48 -0400

    Enabled UNoptimized images in next config to allow for static deployment

--- csmhired/next.config.js
+++ csmhired/next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: './' // req for css stylesheet paths to work
 }
 
 module.exports = nextConfig
