5ddffca0345e873fd0959557ec7bf711ddb443f1
Sand Moshi <sandmoshi@gmail.com>
2022-10-20 00:52:17 -0400

    Updated next.config.js to stop using assetPrefix to prevent 404s when in dev mode

--- csmhired/next.config.js
+++ csmhired/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  assetPrefix: './' // req for css stylesheet paths to work
+  // assetPrefix: './' // req for css stylesheet paths to work - enable this when using npm export
 }
 
 module.exports = nextConfig
