9f51e6489f48aae09e7fcd25b1378b2c98338f80
Sand Moshi <sandmoshi@gmail.com>
2023-01-15 21:20:05 -0500

    Moved images into public/static folder

--- csmhired/constants.js
+++ csmhired/constants.js
@@ -1,10 +1,10 @@
 // imports
-import exampleImage1 from './example/images/dawn-g0a4a5df7a_640.jpg';
-import exampleImage2 from './example/images/night-photograph-g9567df5e9_640.jpg';
-import exampleImage3 from './example/images/pexels-photo-572897.jpeg';
-import exampleImage4 from './example/images/seascape-g20cb2aa3d_1920.jpg';
-import exampleImage5 from './example/images/stars-gac377546d_640.jpg';
-import exampleImage6 from './example/images/taiwan-g64f6b6773_640.jpg';
+import exampleImage1 from 'public/static/images/dawn-g0a4a5df7a_640.jpg';
+import exampleImage2 from 'public/static/images/night-photograph-g9567df5e9_640.jpg';
+import exampleImage3 from 'public/static/images/pexels-photo-572897.jpeg';
+import exampleImage4 from 'public/static/images/seascape-g20cb2aa3d_1920.jpg';
+import exampleImage5 from 'public/static/images/stars-gac377546d_640.jpg';
+import exampleImage6 from 'public/static/images/taiwan-g64f6b6773_640.jpg';
 
 const navLinks = [
     {href:'/', text: 'Jobs'},
