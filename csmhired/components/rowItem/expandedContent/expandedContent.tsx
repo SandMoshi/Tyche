033eda479c96532a928b0fd44eb84648e27768da
Sand Moshi <sandmoshi@gmail.com>
2023-02-04 20:32:44 -0500

    Added content to expanded row item

--- csmhired/components/rowItem/expandedContent/expandedContent.tsx
+++ csmhired/components/rowItem/expandedContent/expandedContent.tsx
@@ -2,9 +2,11 @@
 import { useEffect } from 'react';
 import useMeasure from 'react-use-measure';
 import { animated, useSpring } from '@react-spring/web';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 
 // style imports
 import styles from './expandedContent.module.scss';
+import rowItemStyles from '../rowItem.module.scss';
 
 type Props = {
     isExpanded: boolean,
@@ -18,6 +20,13 @@ const ExpandedContent = (props: Props) => {
     return (
         <animated.div className={styles.animatedContainer} style={animatedHeight}>
             <div className={styles.container}  ref={ref}>
+                <div className={styles.firstRow}>
+                    <p className={rowItemStyles.title}>Job Title</p>
+                    <a className={styles.newTabBtn} href="" target="_blank">
+                        <OpenInNewIcon titleAccess="Open posting in new tab"/>
+                        Open Tab
+                    </a>
+                </div>
                 <p>Extra info here</p>
             </div>
         </animated.div>
