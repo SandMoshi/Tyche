0952950dff95afc6a838d6322d9a471b4b4f6720
Sand Moshi <sandmoshi@gmail.com>
2023-03-30 22:32:23 -0400

    Created basic graphql schemas & apollo test server

--- csmhired/pages/api/graphql.ts
+++ csmhired/pages/api/graphql.ts
@@ -3,53 +3,88 @@ import { startServerAndCreateNextHandler } from "@as-integrations/next";
 
 // Define GraphQL schema
 const typeDefs = `#graphql
-    type Job {
-        title: String,
-        company: String,
+    interface Job {
+        id: ID,
+        company: Company,
+        description: String,
+        salary: Int,
+        currency: String,
         location: String,
-        # TODO - Add all the possible job fields app can query
+        remote: String,
+        datePosted: String,
+        status: String,
     }
 
+    interface Company {
+        id: ID,
+        name: String,
+        location: String,
+        city: String,
+        country: String,
+    }
+
+    type CompanyProfile implements Company {
+        employees: Int,
+        rating: Float,
+        currentlyHiring: Boolean,
+        jobs: [Job]
+    }
+
+    interface User {
+        id: ID,
+        userName: String,
+        picture: ID,
+    }
+
+    type UserAccount implements User {
+        firstName: String,
+        lastName: String,
+        email: String,
+        DOB: {
+            Year: Int,
+            Month: Int,
+            Day: Int,
+        },
+    }
 
     # The "Query" type is special: it lists all of the available queries that
     # clients can execute, along with the return type for each.
-    
+
     type Query {
-        getJobs: [Job],
-        helloWorld: String,
+        jobs: [Job],
+        job(id: ID): Job,
+        users: [User],
+        user(id: ID): [UserAccount],
+        companies: [Company],
+        company(id: ID): [CompanyProfile]
     }
 `;
 
-// Example data-set
-// TODO replace this with data from db
-const jobs = [
-  {
-    title: "job1",
-    company: "company1",
-    location: "location1",
-  },
-  {
-    title: "job2",
-    company: "company2",
-    location: "location2",
-  },
-  {
-    title: "job3",
-    company: "company3",
-    location: "location3",
-  },
-];
+const jobs: any[] = []; // TODO - get data from DB
+const users: any[] = []; // TODO - get data from DB
+const companies: any[] = []; // TODO - get data from DB
 
 // Define resolvers
 // Resolvers define how to fetch the types defined in your schema.
 
 const resolvers = {
   Query: {
-    getJobs: () => jobs,
-    helloWorld: () => "Hello Tyche!!",
+    jobs: () => jobs,
+    job(parent, args, contextValue, info) {
+      return jobs.find((job) => job.id === args.id);
+    },
+    users: () => users,
+    user(parents, args, contextValue, info) {
+      return users.find((user) => user.id === args.id);
+    },
+    companies: () => companies,
+    company(parents, args, contextValue, info) {
+      return companies.find((company) => company.id === args.id);
+    },
   },
 };
 
+// APOLLO SERVER
 const server = new ApolloServer({
   typeDefs,
   resolvers,
